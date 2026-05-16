a2f691984cd2e75486c39336a72bc8d8529dfe45
youssefnjm <youssefnoujoum04@gmail.com>
2026-05-16 14:03:57 +0100

    add certification

--- src/page/CertifceSection.jsx
+++ src/page/CertifceSection.jsx
@@ -2,25 +2,32 @@ import GradientText from "../Components/GradientText";
 import CertificationC from '../assets/Certifes/Certification C.jpg';
 import CertificationJs from '../assets/Certifes/Certification js.jpg';
 import CertificationPhp from '../assets/Certifes/Certification php.jpg';
+import CertificationCpp from '../assets/Certifes/Certification C++.png';
 
 export default function CertifceSection() {
+
+	const certifce = [
+		{
+			title : "javascript Certification",
+			img : CertificationJs,
+		},
+		{
+			title : "php Certification",
+			img : CertificationPhp,
+		},
+		{
+			title : "C Certification",
+			img : CertificationC,
+		},
+		{
+			title : "C++ Certification",
+			img : CertificationCpp,
+		},
+	];
 
 	return (<>
 		<div className='container m-auto w-full py-10 md:py-20' id="certifce">
 			<div className='pl-1.5- w-full items-start mb-10'>
-				{/* <GradientText
-					colors={[
-						"#C20A0A", // main red
-						"#FF6B6B", // lighter red/pink accent
-						"#FFD93D", // golden yellow for contrast
-						"#FF6B6B", // lighter red/pink accent
-						"#C20A0A", // main red
-					]}
-					animationSpeed={5}
-					showBorder={false}
-				>
-					My Certifce
-				</GradientText> */}
                 {/* ── Top label ── */}
                 <div className="flex items-center gap-3 mb-6">
                 <span className="block w-8 h-px bg-purple-400" />
@@ -39,24 +46,16 @@ export default function CertifceSection() {
                 </div>
 			</div>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-				<div className="relative p-2 bg-zinc-900/50 border border-zinc-800 rounded-2xl hover:border-zinc-600 transition-all duration-300">
-					<img className="rounded-t-lg w-full h-fit" src={CertificationC} alt="" />
-					<div className="p-5">
-						<h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white" style={{lineHeight: "1.3", letterSpacing: "-0.02em"}}> C Certification </h5>
-					</div>
-				</div>
-				<div className="relative p-2 bg-zinc-900/50 border border-zinc-800 rounded-2xl hover:border-zinc-600 transition-all duration-300">
-					<img className="rounded-t-lg w-full h-fit" src={CertificationJs} alt="" />
-					<div className="p-5">
-						<h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white" style={{lineHeight: "1.3", letterSpacing: "-0.02em"}}> javascript Certification </h5>
-					</div>
-				</div>
-				<div className="relative p-2 bg-zinc-900/50 border border-zinc-800 rounded-2xl hover:border-zinc-600 transition-all duration-300">
-					<img className="rounded-t-lg w-full h-fit" src={CertificationPhp} alt="" />
-					<div className="p-5">
-						<h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white" style={{lineHeight: "1.3", letterSpacing: "-0.02em"}}>php Certification </h5>
-					</div>
-				</div>
+				{certifce.map((ele, key) => {
+					return (
+						<div key={key} className="relative p-2 bg-zinc-900/50 border border-zinc-800 rounded-2xl hover:border-zinc-600 transition-all duration-300">
+							<img className="rounded-t-lg w-full h-fit" src={ele.img} alt="" />
+							<div className="p-5">
+								<h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white" style={{lineHeight: "1.3", letterSpacing: "-0.02em"}}>{ele.title}</h5>
+							</div>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	</>);
